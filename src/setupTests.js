27846a0f25b6f8c700918d5abdf86f3708aa63f8
Júlia Tallarico <julia.tallcaroline@outlook.com>
2024-09-27 22:34:41 -0300

    chore: deleted issues code

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-undef */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
+// Adicione isso no setupTests.js ou no topo do arquivo de teste
+import { TextEncoder, TextDecoder } from "util";
+
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
