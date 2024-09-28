fe60dbe39ffa8f62c5ef02a64b7b69d979b805a0
Júlia Tallarico <julia.tallcaroline@outlook.com>
2024-09-27 22:49:35 -0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser, // Definindo o ambiente como 'browser'
+    },
+  },
+  pluginJs.configs.recommended, // Configuração recomendada do ESLint para JavaScript
+  pluginReact.configs.flat.recommended, // Configuração recomendada do ESLint para React
 ];
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// xtest("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("verifica se o link 'learn react' está presente", () => {
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
